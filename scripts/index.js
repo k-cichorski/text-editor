2622a204306ea4bdefda7b7f877753fc5ed1e403
jakub.cichorski@hotmail.com <jakub.cichorski@hotmail.com>
2021-05-07 13:40:11 +0200

    buttons indicate their pressed state, button state changes accordingly to caret positon

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,33 @@
 const textBox = document.getElementById('textbox-input');
 
-const execCommand = (command, arguments, showDefaultUI=false) => {
-  document.execCommand(command, showDefaultUI, arguments);
-}
+const execCommand = (button, arguments=null, showDefaultUI=false) => {
+  document.execCommand(button.action, showDefaultUI, arguments);
+};
 
 const getButtons = () => {
   return [...document.getElementById('editor-toolbar').children].map(node => {
     return {
       el: node,
-      action: node.dataset.command
+      action: node.dataset.command,
+      pressed: false
     };
   });
+};
+
+const toggleButtonPressed = (button) => {
+  button.pressed = !button.pressed;
+  button.el.classList.toggle('pressed');
 };
 
 const applyHandlers = buttons => {
   buttons.forEach(button => {
     if (button.el) {
       button.el.onmousedown = e => e.preventDefault();
-      button.action && (button.el.onclick = () => execCommand(button.action));
+      button.action && (button.el.onclick = () => {
+        toggleButtonPressed(button);
+        textBox.focus();
+        execCommand(button);
+      });
     }
   });
 };
@@ -26,7 +36,7 @@ const applyHandlers = buttons => {
 const insertTabIfEditing = e => {
   if (e.code == 'Tab' && document.activeElement == textBox) {
     e.preventDefault();
-    execCommand('insertText', '\t');
+    document.execCommand('insertText', false, '\t');
   }
 };
 
@@ -36,10 +46,21 @@ const removeLoadingMask = () => {
   setTimeout(() => {
     loadingMask.remove();
   }, 400);
+};
+
+const adjustButtonPressed = () => {
+  const adjustButton = (button) => {
+    const hasStyling = document.queryCommandState(button.action);
+    if ((hasStyling && !button.pressed) || (!hasStyling && button.pressed)) {
+      toggleButtonPressed(button);
+    }
+  }
+  buttons.forEach(adjustButton);
 }
 
 let buttons = getButtons();
 applyHandlers(buttons);
 
 document.addEventListener('keydown', insertTabIfEditing);
+document.onselectionchange = adjustButtonPressed;
 window.onload = removeLoadingMask;
